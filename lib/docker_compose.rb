ef06c7f182b24abf61482cdb709b31eb71ad0268
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-26 14:08:36 +0100

    Allow using ERB in stack.yml

--- lib/docker_compose.rb
+++ lib/docker_compose.rb
@@ -1,5 +1,6 @@
 require 'bundler/setup'
 require 'yaml'
+require 'erb'
 
 require "active_support/core_ext/hash/indifferent_access"
 require "active_support/core_ext/hash/deep_merge"
@@ -12,7 +13,7 @@ loader.setup
 module DockerCompose
   def self.build_stack
     architecture = Architecture.new("./docker-compose-full.yml")
-    stack = Stack.new(YAML.load_file("./stack.yml"), using: architecture)
+    stack = Stack.new(YAML.safe_load(ERB.new(File.read("./stack.yml")).result), using: architecture)
     stack.write_docker_compose
   end
 end
