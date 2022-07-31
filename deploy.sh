8601a2479356f2070a892783f0cc282982094e4a
matiasmiodosky <matias.miodosky@emilabs.ai>
2022-07-31 12:27:18 -0300

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 # install git
+sudo apt-get update
+
 
 sudo apt install git-all -y
 
@@ -18,7 +20,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
